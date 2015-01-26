020f6bdb6f5be0874f9416067f6d9722be77571e
U-idea-PC\zht <zht@idea-PC.(none)>
2015-01-26 21:49:16 +0800

    Tab to space

--- src/ProductInfo.java
+++ src/ProductInfo.java
@@ -1,29 +1,29 @@
 
 public class ProductInfo {
-	private String name;
-	private double price;
-	private String unit;
-	private boolean isPromotion;
-	
-	public ProductInfo(String name, double price, String unit, boolean isPromotion) {
-		this.name = name;
-		this.price = price;
-		this.unit = unit;
-		this.isPromotion = isPromotion;
-	}
-	public String getUnit() {
-		return unit;
-	}
+    private String name;
+    private double price;
+    private String unit;
+    private boolean isPromotion;
+    
+    public ProductInfo(String name, double price, String unit, boolean isPromotion) {
+        this.name = name;
+        this.price = price;
+        this.unit = unit;
+        this.isPromotion = isPromotion;
+    }
+    public String getUnit() {
+        return unit;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public double getPrice() {
-		return price;
-	}
+    public double getPrice() {
+        return price;
+    }
 
-	public boolean isPromotion() {
-		return isPromotion;
-	}
+    public boolean isPromotion() {
+        return isPromotion;
+    }
 }
--- src/ProductInfoFactory.java
+++ src/ProductInfoFactory.java
@@ -1,14 +1,14 @@
 
 public class ProductInfoFactory {
-	public static ProductInfo getProductInfo(String itemID) {
-		if ("ITEM000005".equals(itemID)){
-			return new ProductInfo("可口可乐", 3.00, "瓶", true);
-		} else if ("ITEM000001".equals(itemID)){
-			return new ProductInfo("羽毛球", 1.00, "个", true);
-		} else if ("ITEM000003".equals(itemID)){
-			return new ProductInfo("苹果", 5.50, "斤", false);
-		} else{
-			return null;
-		}
-	}
+    public static ProductInfo getProductInfo(String itemID) {
+        if ("ITEM000005".equals(itemID)){
+            return new ProductInfo("可口可乐", 3.00, "瓶", true);
+        } else if ("ITEM000001".equals(itemID)){
+            return new ProductInfo("羽毛球", 1.00, "个", true);
+        } else if ("ITEM000003".equals(itemID)){
+            return new ProductInfo("苹果", 5.50, "斤", false);
+        } else{
+            return null;
+        }
+    }
 }
